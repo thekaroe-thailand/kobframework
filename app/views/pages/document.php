1cd958ea1611bc8810886a6af350a4295cf2e334
thekaroe-thailand <thekaroe@gmail.com>
2024-03-01 14:36:18 +0700

    core

--- app/views/pages/document.php
+++
@@ -1,8 +0,0 @@
-<?php require_once 'app/views/components/navbar.php'; ?>
-
-<section>
-    <div class="container">
-        <h3>Document</h3>
-        <h5>Comming Soon...</h5>
-    </div>
-</section>
