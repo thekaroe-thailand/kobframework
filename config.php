7181143e74c2eb9473ce422473d4efa4b6e07ee0
thekaroe-thailand <thekaroe@gmail.com>
2024-03-03 12:06:31 +0700

    new config file for connet database and Model for ORM in future.

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+$db_provider = 'mysql';
+$db_username = 'root';
+$db_password = 'root';
+$db_host = 'localhost';
+$db_port = 3306;
+$db_name = 'db_kobframework';
+
+try {
+    $db = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8", $db_username, $db_password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
---
+++ libs/Model.php
@@ -0,0 +1,26 @@
+<?php
+
+class Model
+{
+    protected function findMany()
+    {
+    }
+    protected function findOne()
+    {
+    }
+    protected function create($data = [])
+    {
+    }
+    protected function update($data = [], $where)
+    {
+    }
+    protected function updateMany($data = [], $where = [])
+    {
+    }
+    protected function delete($where)
+    {
+    }
+    protected function deleteMany($where = [])
+    {
+    }
+}
