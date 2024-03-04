0d79d1772b268ad7b8b0778e33579ef5b6d42d73
thekaroe-thailand <thekaroe@gmail.com>
2024-03-04 13:30:44 +0700

    create global variable for use ref path and use db

--- config.php
+++ config.php
@@ -3,7 +3,8 @@
 //
 // web path from /var/www/htdocs
 //
-$webPath = '/kobframework-website';
+define('PATH', '/kobframework-website');
+define("CONNECT_DB", false);
 
 //
 // database
@@ -15,9 +16,11 @@ $db_host = 'localhost';
 $db_port = 3306;
 $db_name = 'db_kobframework';
 
-try {
-    $db = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8", $db_username, $db_password);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo $e->getMessage();
+if (CONNECT_DB) {
+    try {
+        $db = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8", $db_username, $db_password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
 }
